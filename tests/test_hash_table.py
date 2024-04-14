aad8b278fcbcca88a6a7d3746563b35ddccd3571
WVAviator <wvaviator@gmail.com>
2024-04-13 20:20:12 -0500

    create hash table tests

---
+++ tests/test_hash_table.py
@@ -0,0 +1,14 @@
+from utilities.hash_table import HashTable
+
+def test_basic_usage():
+    hash_table = HashTable()
+    hash_table.insert('foo', 'bar')
+    assert hash_table.get('foo') == 'bar'
+
+def test_collisions():
+    hash_table = HashTable()
+    hash_table.insert(0, 'foo')
+    hash_table.insert(40, 'bar')
+    assert hash_table.get(0) == 'foo'
+    assert hash_table.get(40) == 'bar'
+
