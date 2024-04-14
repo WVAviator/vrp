50c18699def3fe156279c1571d3b95c3eea94036
WVAviator <wvaviator@gmail.com>
2024-04-13 20:20:28 -0500

    create package table reading from Excel file

---
+++ tests/test_package_table.py
@@ -0,0 +1,16 @@
+from wgups.package_table import PackageTable
+
+def test_package_table():
+    package_table = PackageTable("resources/WGUPS Package File.xlsx")
+    package1 = package_table.get_package(1)
+    package40 = package_table.get_package(40)
+
+    assert package1.package_id == 1
+    assert package1.address == "195 W Oakland Ave"
+    assert package1.weight == 21
+    assert package1.status == "at the hub"
+
+    assert package40.package_id == 40
+    assert package40.address == "380 W 2880 S"
+    assert package40.weight == 45
+    assert package40.status == "at the hub"
---
+++ wgups/package_table.py
@@ -0,0 +1,50 @@
+from utilities.hash_table import HashTable
+from typing import cast
+from openpyxl import load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+class Package:
+    def __init__(self, package_id: int, address: str, deadline: str, city: str, zip_code: str, weight: int, note: str):
+        self.package_id = package_id
+        self.address = address
+        self.deadline = deadline
+        self.city = city
+        self.zip_code = zip_code
+        self.weight = weight
+        self.note = note
+        self.status = "at the hub"
+
+class PackageTable:
+    def __init__(self, package_file_path: str):
+        self._package_table = HashTable()
+        self._load_package_data(package_file_path)
+
+    def get_package(self, package_id: int) -> Package:
+        return self._package_table.get(package_id)
+
+    def _load_package_data(self, package_file_path: str):
+        wb = load_workbook(package_file_path)
+
+        if wb is None:
+            raise Exception("Package file not found")
+
+        ws = cast( Worksheet, wb.active )
+
+        for row in ws.iter_rows(min_row=9, max_row=ws.max_row, min_col=1, max_col=8):
+
+            row_values = []
+            for cell in row:
+                row_values.append(cell.value)
+
+            package_id = int(row_values[0])
+            address = row_values[1]
+            deadline = row_values[5]
+            city = row_values[2]
+            zip_code = row_values[4]
+            weight = int(row_values[6])
+            note = row_values[7]
+
+            package = Package(package_id, address, deadline, city, zip_code, weight, note)
+
+            self._package_table.insert(package_id, package)
+        
